f6a801bdd5697df3100fed3bba9330535bffcd03
NoBugsForGit <zhangjuncpp@outlook.com>
2024-07-23 11:26:09 +0800

    兼容安卓端

--- assets/Script/Player.ts
+++ assets/Script/Player.ts
@@ -1,41 +1,36 @@
-import { _decorator, Component, Node, Input, input, EventKeyboard, KeyCode, Vec3, sp } from 'cc';
+import { _decorator, Component, Node, Input, input, EventKeyboard, KeyCode, Vec3, sp, EventTouch } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('Player')
 export class Player extends Component {
-    @property 
+    @property
     public SPEED_ADD: number = 100;  //速度增加值
-    @property 
+    @property
     public GRIV: number = 10; //重力加速度
-    @property 
+    @property
     public speedY: number = 1000;
-    @property 
-    public initPositionX : number = -500;
-    @property 
-    public speedMultiple : number = 5;
+    @property
+    public initPositionX: number = -500;
+    @property
+    public speedMultiple: number = 5;
     start() {
         this.node.setPosition(this.initPositionX, 0);
-        input.on(Input.EventType.KEY_DOWN, this.ifKeyBoard, this);
+        input.on(Input.EventType.TOUCH_START, this.ifKeyBoard, this);
     }
 
     protected onDestroy(): void {
-        input.off(Input.EventType.KEY_DOWN, this.ifKeyBoard, this);
+        input.off(Input.EventType.TOUCH_START, this.ifKeyBoard, this);
     }
 
-    ifKeyBoard(event: EventKeyboard) {
-        console.log("press SPACE!");
-        switch (event.keyCode) {
-            case KeyCode.SPACE:
-                this.speedY += this.SPEED_ADD;
-                break;
-        }
+    ifKeyBoard(event: EventTouch) {
+        this.speedY += this.SPEED_ADD;
     }
 
     update(deltaTime: number) {
         this.speedY -= this.GRIV;
         let position = new Vec3;
         this.node.getPosition(position);
-        position.y += this.speedY * this.speedMultiple * deltaTime ;
+        position.y += this.speedY * this.speedMultiple * deltaTime;
 
         this.node.setPosition(position);
 
